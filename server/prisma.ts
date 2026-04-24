049020072c7b8f76ee590a852654c801fb1fab81
Ricardo Morais <ricardomorais@MacBook-Pro-de-Ricardo.local>
2026-04-24 13:20:02 -0300

    Refactoring

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,20 +1,13 @@
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import { serverEnv } from './env.js';
 
 declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined;
 }
 
-const adapter = new PrismaPg({
-  connectionString: serverEnv.databaseUrl,
-});
-
 export const prisma =
   globalThis.__prisma ||
   new PrismaClient({
-    adapter,
     log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
   });
 
